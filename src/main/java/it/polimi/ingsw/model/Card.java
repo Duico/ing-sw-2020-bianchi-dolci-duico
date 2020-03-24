182335b741626c28213db94b06250d026bbe591d
Francesco Dolci <dolcifred@gmail.com>
2020-03-24 22:05:56 +0100

    create model

--- src/main/java/it/polimi/ingsw/model/Card.java
+++ src/main/java/it/polimi/ingsw/model/Card.java
@@ -1,163 +1,67 @@
 package it.polimi.ingsw.model;
 
-import it.polimi.ingsw.model.exception.StrategyNameNotFound;
-import it.polimi.ingsw.model.strategy.*;
-
-import java.io.Serializable;
-import java.util.Objects;
-
-/**
- * Represents a God card with strategies to be used by the worker
- */
-public class Card implements Serializable {
-    private final String name;
+public class Card {
+    private String name;
     private MoveStrategy moveStrategy;
     private BuildStrategy buildStrategy;
     private WinStrategy winStrategy;
     private BlockStrategy blockStrategy;
     private OpponentStrategy opponentStrategy;
 
-    /**
-     *
-     * @param name Name of the God
-     * @param moveStrategy  Name of the strategy for movement
-     * @param buildStrategy Name of the strategy for building
-     * @param winStrategy Name of the strategy for winning
-     * @param blockStrategy Name of the strategy for blocking next player's movement
-     * @param opponentStrategy Name of the strategy for opponents that occupy the desired cell
-     */
-    public Card (String name, String moveStrategy, String buildStrategy, String winStrategy, String blockStrategy, String opponentStrategy) throws StrategyNameNotFound {
+    public Card(String name, MoveStrategy moveStrategy, BuildStrategy buildStrategy, WinStrategy winStrategy, BlockStrategy blockStrategy, OpponentStrategy opponentStrategy){
         this.name=name;
-        setMoveStrategy(moveStrategy);
-        setBuildStrategy(buildStrategy);
-        setBlockStrategy(blockStrategy);
-        setOpponentStrategy(opponentStrategy);
-        setWinStrategy(winStrategy);
-
-    }
-
-    public static Card getDefaultCard(){
-        try {
-            Card defaultCard = new Card("Default", "Default", "Default", "Default", "Default", "Default");
-            return defaultCard;
-        }catch(StrategyNameNotFound e){
-            throw new RuntimeException("Couldn't create default Card");
-        }
+        this.moveStrategy=moveStrategy;
+        this.buildStrategy=buildStrategy;
+        this.winStrategy=winStrategy;
+        this.blockStrategy=blockStrategy;
+        this.opponentStrategy=opponentStrategy;
     }
 
     public String getName() {
         return name;
     }
-    public MoveStrategy getMoveStrategy() {
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMoveStrategy() {
         return moveStrategy;
     }
-    public BuildStrategy getBuildStrategy() {
+
+    public void setMoveStrategy(String moveStrategy) {
+        this.moveStrategy = moveStrategy;
+    }
+
+    public String getBuildStrategy() {
         return buildStrategy;
     }
-    public WinStrategy getWinStrategy() {
+
+    public void setBuildStrategy(String buildStrategy) {
+        this.buildStrategy = buildStrategy;
+    }
+
+    public String getWinStrategy() {
         return winStrategy;
     }
-    public BlockStrategy getBlockStrategy() {
+
+    public void setWinStrategy(String winStrategy) {
+        this.winStrategy = winStrategy;
+    }
+
+    public String getBlockStrategy() {
         return blockStrategy;
     }
-    public OpponentStrategy getOpponentStrategy() {
+
+    public void setBlockStrategy(String blockStrategy) {
+        this.blockStrategy = blockStrategy;
+    }
+
+    public String getOpponentStrategy() {
         return opponentStrategy;
     }
 
-    /*public void setName(String name) {
-        this.name = name;
-    }*/
-
-    private void setMoveStrategy(String moveStrategy) throws StrategyNameNotFound {
-
-        if (moveStrategy.equals("Default")){
-            this.moveStrategy = new DefaultMove();
-        }
-        else if(moveStrategy.startsWith("Multiple")){
-            int numMoves = Integer.parseUnsignedInt(moveStrategy.substring("Multiple".length()).replaceAll("[\\D]", ""));
-            if(numMoves>1 && numMoves<=8) {
-                this.moveStrategy = new MultipleMove(numMoves);
-            }else
-                throw new StrategyNameNotFound();
-        }else if(moveStrategy.contentEquals("Push")){
-            this.moveStrategy = new PushMove();
-        }
-        else if(moveStrategy.contentEquals("BuildAtFirst")){
-            this.moveStrategy = new BuildAtFirstMove();
-        }else {
-            throw new StrategyNameNotFound();
-        }
+    public void setOpponentStrategy(String opponentStrategy) {
+        this.opponentStrategy = opponentStrategy;
     }
-
-    private void setBuildStrategy(String buildStrategy) throws StrategyNameNotFound {
-
-        if (buildStrategy.contentEquals("Default")){
-            this.buildStrategy = new DefaultBuild();
-        }
-        else if (buildStrategy.contentEquals("Dome")){
-            this.buildStrategy = new DomeBuild();
-        }
-        else if (buildStrategy.contentEquals("Double")){
-            this.buildStrategy = new DoubleBuild();
-        }
-        else if(buildStrategy.contentEquals("DifferentMultiple")) {
-            this.buildStrategy = new MultipleBuildDifferentPosition();
-        }
-        else if (buildStrategy.contentEquals("BuildAtFirst")){
-            this.buildStrategy = new BuildAtFirstBuild();
-        }else {
-            throw new StrategyNameNotFound();
-        }
-    }
-
-    private void setWinStrategy(String winStrategy) throws StrategyNameNotFound {
-        if (winStrategy.contentEquals("Default")){
-            this.winStrategy = new DefaultWin();
-        }
-        else if (winStrategy.contentEquals("DownMove")){
-            this.winStrategy = new DownMoveWin();
-        }else {
-            throw new StrategyNameNotFound();
-        }
-    }
-
-    private void setBlockStrategy(String blockStrategy) throws StrategyNameNotFound {
-        if (blockStrategy.contentEquals("Default")){
-            this.blockStrategy = new DefaultBlock();
-        }
-        else if (blockStrategy.contentEquals("UpMove")){
-            this.blockStrategy = new UpMoveBlock();
-        }else {
-            throw new StrategyNameNotFound();
-        }
-    }
-
-    private void setOpponentStrategy(String opponentStrategy) throws StrategyNameNotFound {
-
-        if (opponentStrategy.contentEquals("Default")){
-            this.opponentStrategy = new DefaultOpponent();
-        }
-        else if (opponentStrategy.contentEquals("Swap")){
-            this.opponentStrategy = new SwapOpponent();
-        }
-        else if (opponentStrategy.contentEquals("Push")){
-            this.opponentStrategy = new PushOpponent();
-        }else {
-            throw new StrategyNameNotFound();
-        }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Card card = (Card) o;
-        return Objects.equals(getName(), card.getName()) &&
-                Objects.equals(getMoveStrategy().getClass(), card.getMoveStrategy().getClass()) &&
-                Objects.equals(getBuildStrategy().getClass(), card.getBuildStrategy().getClass()) &&
-                Objects.equals(getBlockStrategy().getClass(), card.getBlockStrategy().getClass()) &&
-                Objects.equals(getOpponentStrategy().getClass(), card.getOpponentStrategy().getClass()) &&
-                Objects.equals(getWinStrategy().getClass(), card.getWinStrategy().getClass());
-    }
-
 }
--- src/main/java/it/polimi/ingsw/model/CardDeck.java
+++ src/main/java/it/polimi/ingsw/model/CardDeck.java
@@ -1,8 +1,9 @@
 package it.polimi.ingsw.model;
 
-import it.polimi.ingsw.model.exception.ReadConfigurationXMLException;
-import it.polimi.ingsw.model.exception.StrategyNameNotFound;
-import org.w3c.dom.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -10,31 +11,28 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 
-public class CardDeck implements Serializable {
+public class CardDeck {
     private ArrayList<Card> cardDeck;
 
-    public CardDeck() throws IOException, SAXException, ParserConfigurationException, ReadConfigurationXMLException {
-        this("./card-config.xml");
-    }
-
-    public CardDeck(String pathname) throws IOException, SAXException, ParserConfigurationException, ReadConfigurationXMLException{
+    public CardDeck() throws IOException, SAXException, ParserConfigurationException {
         cardDeck = new ArrayList<>();
-        readConfigurationXML(pathname);
+        readConfigurationXML();
     }
 
-
-
-    private void readConfigurationXML(String pathname) throws ReadConfigurationXMLException, ParserConfigurationException, IOException, SAXException {
+    private void readConfigurationXML() throws ParserConfigurationException, IOException, SAXException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
 
-        Document document = builder.parse(new File(pathname));
+        Document document = builder.parse(new File("CardDeck.xml"));    //toDo
+
         document.getDocumentElement().normalize();
+
+        Element root = document.getDocumentElement();
+
         NodeList nList = document.getElementsByTagName("card");
+
 
         for (int temp = 0; temp < nList.getLength(); temp++)
         {
@@ -42,80 +40,40 @@ public class CardDeck implements Serializable {
             if (node.getNodeType() == Node.ELEMENT_NODE)
             {
                 Element eElement = (Element) node;
-                String name = getPropertyName(eElement, "name");
-                String moveStrategy = getPropertyName(eElement, "moveStrategy");
-                String buildStrategy = getPropertyName(eElement, "buildStrategy");
-                String winStrategy = getPropertyName(eElement,"winStrategy");
-                String blockStrategy = getPropertyName(eElement, "blockStrategy");
-                String opponentStrategy = getPropertyName(eElement,"opponentStrategy");
+                String name = eElement.getElementsByTagName("name").item(0).getTextContent();
+                String moveStrategy = eElement.getElementsByTagName("moveStrategy").item(0).getTextContent();
+                String buildStrategy = eElement.getElementsByTagName("buildStrategy").item(0).getTextContent();
+                String winStrategy = eElement.getElementsByTagName("winStrategy").item(0).getTextContent();
+                String blockStrategy = eElement.getElementsByTagName("blockStrategy").item(0).getTextContent();
+                String opponentStrategy = eElement.getElementsByTagName("opponentStrategy").item(0).getTextContent();
 
-                try {
-                    cardDeck.add(new Card(name, moveStrategy, buildStrategy, winStrategy, blockStrategy, opponentStrategy));
-                }catch (StrategyNameNotFound e){
-                    System.err.println("Strategy name not found. Check your XML file.");
-                    e.printStackTrace();
-                }
+                cardDeck.add(new Card(name, moveStrategy, buildStrategy, winStrategy, blockStrategy, opponentStrategy));
+
             }
-        }
-        if(cardDeck.size() < 3){ //FIX too vague
-            throw new ReadConfigurationXMLException();
         }
     }
 
     public Card getCard(int i) {
         return cardDeck.get(i);
     }
-    public List<String> getCardNames(){
-        ArrayList<String> cardNames  = new ArrayList<String>();
-        for(Card card: cardDeck){
-            cardNames.add(card.getName());
+
+    public ArrayList<Card> pickRandom(int numPlayers)
+    {
+        int rand;
+        ArrayList<Card> deck = new ArrayList<>();
+        int i = 0;
+        while(i < numPlayers)
+        {
+            Card randCard ;
+            do {
+                rand = (int) (Math.random() * (cardDeck.size() + 1));
+                randCard=cardDeck.get(rand);
+            }
+            while(deck.contains(randCard));
+            deck.add(randCard);
+            i++;
         }
-        return cardNames;
-    }
-
-//    /**
-//     * Picks [numPlayers] random cards to be assigned to all players in the game
-//     * @param numPlayers Number of cards to extract,should be equal to the number of players
-//     * @return Deck of [numPlayers] cards
-//     */
-//    public ArrayList<Card> pickRandom(int numPlayers) {
-//        int rand;
-//        ArrayList<Card> randomDeck = new ArrayList<>();
-//        int i = 0;
-//        while(i < numPlayers)
-//        {
-//            Card randCard ;
-//            do {
-//                rand = (int) Math.floor( (Math.random() * (double) cardDeck.size()) );
-//                randCard=cardDeck.get(rand);
-//            }
-//            while(randomDeck.contains(randCard));
-//            randomDeck.add(randCard);
-//            i++;
-//        }
-//        return randomDeck;
-//    }
-
-    public boolean existsCardByName(String name){
-        for(int i=0; i<cardDeck.size();i++){
-            if(name.equals(cardDeck.get(i).getName()))
-                return true;
-        }
-        return false;
-    }
-
-    public Card getCardByName(String name){
-        int i;
-        for(i=0; i<cardDeck.size();i++){
-            if(name.equals(cardDeck.get(i).getName()))
-                break;
-        }
-        return cardDeck.get(i);
-
-    }
-    private String getPropertyName(Element node, String tagName){
-        Node leaf = node.getElementsByTagName(tagName).item(0);
-        return (leaf != null)? leaf.getTextContent():"Default";
+        return deck;
     }
 }
 
