b3d995398ce7e4189cb2065e3ffa84135fd55ce4
Duico <10580381@polimi.it>
2020-04-20 19:11:28 +0200

    Finish FixedArrayTest

--- src/test/java/it/polimi/ingsw/model/FixedArrayTest.java
+++ src/test/java/it/polimi/ingsw/model/FixedArrayTest.java
@@ -23,7 +23,7 @@ class FixedArrayTest {
         });
     }
     @Test
-    void add() {
+    void fullAddGet() {
         FixedArray<Worker> fa = new FixedArray<>(4);
         Worker w1 = new Worker();
         assertEquals(0, fa.add(w1));
@@ -44,7 +44,7 @@ class FixedArrayTest {
         assertSame(w3, fa.get(2));
         assertNull(fa.get(3));
         Worker w4 = new Worker();
-        assertEquals(3, fa.add(w3));
+        assertEquals(3, fa.add(w4));
         assertSame(w1, fa.get(0));
         assertSame(w2, fa.get(1));
         assertSame(w3, fa.get(2));
@@ -54,14 +54,58 @@ class FixedArrayTest {
     }
 
     @Test
-    void get() {
-    }
-
-    @Test
-    void set() {
-    }
-
-    @Test
     void size() {
+        FixedArray<Worker> fa = new FixedArray<>(4);
+        assertEquals(4, fa.size());
+        Worker w1 = new Worker();
+        assertEquals(0, fa.add(w1));
+        assertEquals(4, fa.size());
+    }
+
+    @Test
+    void nullCount(){
+        int size = 4;
+        FixedArray<Worker> fa = new FixedArray<>(size);
+        Worker w1 = new Worker();
+        assertEquals(0, fa.add(w1));
+        assertEquals(size-1, fa.nullCount());
+
+        Worker w2 = new Worker();
+        assertEquals(1, fa.add(w2));
+        assertEquals(size-2, fa.nullCount());
+
+        Worker w3 = new Worker();
+        assertEquals(2, fa.add(w3));
+        assertEquals(size-3, fa.nullCount());
+
+        assertNull(fa.get(3));
+        Worker w4 = new Worker();
+        assertEquals(3, fa.add(w3));
+        assertEquals(size-4, fa.nullCount());
+
+    }
+    @Test
+    void nullCountWithRemoval(){
+        int size = 4;
+        FixedArray<Worker> fa = new FixedArray<>(size);
+        Worker w1 = new Worker();
+        assertEquals(0, fa.add(w1));
+        assertEquals(size-1, fa.nullCount());
+
+        Worker w2 = new Worker();
+        assertEquals(1, fa.add(w2));
+        assertEquals(size-2, fa.nullCount());
+
+        fa.set(1, null);
+
+        Worker w3 = new Worker();
+        assertEquals(1, fa.add(w3));
+        assertEquals(size-2, fa.nullCount());
+
+        assertNull(fa.get(3));
+        Worker w4 = new Worker();
+        assertEquals(2, fa.add(w3));
+        assertEquals(size-3, fa.nullCount());
+
     }
 }
