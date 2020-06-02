48b90294b7755131243b901c9fdbc3df1da5d9ed
Duico <10580381@polimi.it>
2020-06-02 18:08:05 +0200

    Fix BuildingHeight

--- src/main/java/it/polimi/ingsw/client/gui/BuildingHeight.java
+++ src/main/java/it/polimi/ingsw/client/gui/BuildingHeight.java
@@ -2,9 +2,9 @@ package it.polimi.ingsw.client.gui;
 
 public enum BuildingHeight {
     EMPTY(0),
-    BASE(-1.323),
-    MID(-2.434),
-    TOP(-3.034);
+    BASE(-1.153),
+    MID(-2.264),
+    TOP(-2.864);
     private final double height;
     private BuildingHeight(double height){
         this.height = height;
