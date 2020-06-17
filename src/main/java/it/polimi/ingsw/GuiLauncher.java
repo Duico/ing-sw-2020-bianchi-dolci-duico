81634858c8cf6d498c7720d6f275e807cea4b6d1
Your Name <you@example.com>
2020-06-17 17:33:20 +0200

    Change module name, use external gui Launcher

---
+++ src/main/java/it/polimi/ingsw/GuiLauncher.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.client.gui.GuiMessageVisitor;
+import javafx.application.Application;
+
+public class GuiLauncher {
+    public static void main(String[] args) {
+        GuiApp.main(args);
+    }
+}
